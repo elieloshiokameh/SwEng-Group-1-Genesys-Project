76efc0173aed0cdba0242a6648e7a5acd9399b5b
Gregory Manitaras <manitarasg@gmail.com>
2024-02-09 23:09:48 +0000

    Added Spring

--- src/main/java/ie/tcd/sweng2024/groupone/api/ApiApplication.java
+++ src/main/java/ie/tcd/sweng2024/groupone/api/ApiApplication.java
@@ -1,18 +1,24 @@
-package ie.tcd.SwEng2024.GroupOne.API;
+package ie.tcd.sweng2024.groupone.api;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.IOException;
 
-public class Main {
-    public static void main(String[] args) {
+@SpringBootApplication
+public class ApiApplication {
 
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
         try {
             Document doc = Jsoup.connect("https://www.myunidays.com/IE/en-IE/category/technology_all-technology-offers/").get();
             System.out.println(doc.body().toString());
         } catch (IOException e) {
             System.out.println("Could not fetch website!");
         }
+
     }
+
 }
---
+++ src/test/java/ie/tcd/sweng2024/groupone/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.tcd.sweng2024.groupone.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
