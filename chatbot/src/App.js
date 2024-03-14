4301dc751eeee062b8da6e2aaf484c429bfc56e2
Jack Kearney <kearnej8@tcd.ie>
2024-03-14 14:31:14 +0000

    added sample images

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import Navbar from './components/Navbar';
+import Home from './components/Home';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
 
 function App() {
-  return <div classname="App">Hello</div>;
+  return <div classname="App">
+    <Home/>
+  </div>;
 }
 
 export default App;
--- chatbot/src/components/Navbar.js
+++ chatbot/src/components/Navbar.js
@@ -2,24 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom'; // assuming you are using react-router for navigation
 
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      {/* logo or brand name */}
-      <div className="navbar-brand">
-        <Link to="/" classname="navbar-logo">DiscountBot
-        Jack
-        </Link >
-      </div>
-
-      {/* navigation links */}
-      <div className="navbar-menu">
-        <Link to="/about">About</Link>
-        <Link to="/services">Services</Link>
-        <Link to="/contact">Contact</Link>
-        {/* Add additional links or components here */}
-      </div>
-    </nav>
-  );
+  return <div>Navbar</div>;
 };
 
 export default Navbar;
