cd59ef882e5209504403e1d24c8808c82c178c2a
Eliel Oshiokameh <eliel.oshiokameh@gmail.com>
2024-04-08 15:50:09 +0100

    integrate chatbot into frontend

--- chatbot/src/components/Discounts.js
+++ chatbot/src/components/Discounts.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import Navbar from './Navbar';
 
 const Discounts = () => {
   return (
-    <div classname="home-container">
-      <Navbar/>
+    <div className="home-container">
+      <Helmet>
+        <script src="https://apps.mypurecloud.ie/genesys-bootstrap/genesys.min.js" type="text/javascript" />
+        <script type="text/javascript">
+          {`
+            window.Genesys = window.Genesys || function () {
+              (window.Genesys.q = window.Genesys.q || []).push(arguments);
+            };
+            window.Genesys.t = 1 * new Date();
+            window.Genesys.c = {
+              environment: 'prod-euw1',
+              deploymentId: 'e0f29402-6636-4e90-98e1-a1c15e5d6663'
+            };
+          `}
+        </script>
+      </Helmet>
+      <Navbar />
+      {/* Your JSX content here */}
     </div>
   );
+};
 
-}
-
-export default Discounts
+export default Discounts;
